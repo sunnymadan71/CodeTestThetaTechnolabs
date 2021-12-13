7db98195ff0dc9d88791de3d53ccad26c747ce88
Sunny Madan <37594983+sunnymadan71@users.noreply.github.com>
2021-12-13 16:57:38 +0530

    CodeTest Project
    Project has been created for the practical round

---
+++ CodeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeTest
+//
+//  Created by logicloops on 13/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
