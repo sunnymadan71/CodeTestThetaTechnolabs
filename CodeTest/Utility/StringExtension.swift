3df100fac7c2c8e12e7e66e71cbf2f6c04b5e8dc
Sunny Madan <37594983+sunnymadan71@users.noreply.github.com>
2021-12-13 20:50:36 +0530

    Code Update

--- CodeTest/Utility/StringExtension.swift
+++
@@ -1,9 +0,0 @@
-
-
-import UIKit
-
-extension String {
-    func trim() -> String {
-        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-    }
-}
--- Pods/Target Support Files/Pods-CodeTest-CodeTestUITests/Pods-CodeTest-CodeTestUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CodeTest_CodeTestUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CodeTest_CodeTestUITests
-@end
